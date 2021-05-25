67c785bedb90f2f23761ccc830d5c7c14c04764a
Pavel T <pa.tomilin@gmail.com>
2021-05-25 23:27:15 +0300

    Настроил Webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  entry: [
+    './js/game.js',
+    './js/stat.js',
+    './js/data.js',
+    './js/util.js',
+    './js/backend.js',
+    './js/script.js',
+    './js/setup.js',
+    './js/render.js',
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname),
+    iife: true,
+  },
+  devtool: false,
+};
